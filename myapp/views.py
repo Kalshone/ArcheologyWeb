4b92971e8e80aa6ec58c11a47d9ef2f0bde77b57
Katherine Pravdin <katherinepravdin@gmail.com>
2024-11-14 19:37:22 -0500

    setup, sidebar and basic styling

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 from .models import Site
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    # return HttpResponse("Hello, Django!")
+    
+    sites = Site.objects.all()
+    
+    # Pass the sites data to the 'home.html' template
+    return render(request, 'home.html', {'sites': sites})
+    
+
