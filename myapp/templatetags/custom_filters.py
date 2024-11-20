2455026755a8d58c6e89075c5ee4db9246f10ed3
Katherine Pravdin <katherinepravdin@gmail.com>
2024-11-20 00:28:00 -0500

    add artifact of the day, recent changes, and documentation blocks on dashboard

--- myapp/templatetags/custom_filters.py
+++ myapp/templatetags/custom_filters.py
@@ -5,7 +5,3 @@ register = template.Library()
 @register.filter
 def attr(obj, attr_name):
     return getattr(obj, attr_name)
-
-# @register.filter
-# def get_site_data(site):
-#     return {field.name: getattr(site, field.name) for field in site._meta.fields}
